0bd1c1c7d384c2439589f5af336f10e5deb499c6
KateSerhiienko <kate.serhiienko@gmail.com>
2022-02-03 14:11:04 +0200

    init

---
+++ gulp/tasks/html.js
@@ -0,0 +1,20 @@
+import webpHtmlNosvg from "gulp-webp-html-nosvg";
+import pug from "gulp-pug";
+
+export const html = () => {
+  return app.gulp.src(app.path.src.html)
+    .pipe(app.plugins.plumber(
+      app.plugins.notify.onError({
+        title: "HTML",
+        message: "Error: <%= error.message %>"
+      })
+    ))
+    .pipe(pug({
+      pretty: true,
+      verbose: true
+    }))
+    .pipe(app.plugins.replace(/@img\//g, 'img/'))
+    .pipe(webpHtmlNosvg())
+    .pipe(app.gulp.dest(app.path.build.html))
+    .pipe(app.plugins.browsersync.stream());
+}
